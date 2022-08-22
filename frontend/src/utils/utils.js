b40719f3ee6eb13ce36f9ea1b66fc3f0230d9631
Jacob <jacob@engd.se>
2022-08-22 22:46:33 +0200

    test 5

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,4 +1,4 @@
-//const BASE_URL="http://localhost:8080";
-const BASE_URL="https://project-pingo.herokuapp.com";
+const BASE_URL="http://localhost:8080";
+//const BASE_URL="https://project-pingo.herokuapp.com";
 
 export const API_URL = (slug) =>`${BASE_URL}/${slug}`;
